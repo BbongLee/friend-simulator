6c5410c6591b72774071826ee31fefcfa8d04538
Yebon Lee <bbong0722@e-mirim.hs.kr>
2017-04-06 20:54:16 +0900

    #10 로그남기기

--- app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
@@ -2,37 +2,49 @@ package kr.hs.emirim.friendsimulatorbbonglee.imchung;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "Blue:Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate 메소드 호출!");
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "activity_main 레이아웃 세팅!");
     }
     void onClick(View view){
+        Log.d(TAG, "onClick 메소드 호출!");
+        Log.d(TAG, "클릭된 뷰아이디!"+ view.getId());
         switch (view.getId()){
             case R.id.button1:
+                Log.d(TAG, "버튼1클릭");
                 Toast.makeText(this, "썸바디 헬ㅍ믜~~~!!~!~!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
+                Log.d(TAG, "버튼2클릭");
                 Toast.makeText(this, "모해~? 지금은~?!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
+                Log.d(TAG, "버튼3클릭");
                 Toast.makeText(this, "여기는 지옥인걸까....?", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
+                Log.d(TAG, "버튼4클릭");
                 Toast.makeText(this, "하ㅣㅎ갛가학핳각학", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
+                Log.d(TAG, "버튼5클릭");
                 Toast.makeText(this, "지려따리~ 오져따리~ ! ! !", Toast.LENGTH_SHORT).show();
                 break;
             default:
+                Log.d(TAG, "모르는버튼클릭");
                 Toast.makeText(this, "뭐라구여?!!?!?!?", Toast.LENGTH_SHORT).show();
                 break;
-
         }
+        Log.d(TAG, "onClick메소드 무사히종료!");
     }
 }
