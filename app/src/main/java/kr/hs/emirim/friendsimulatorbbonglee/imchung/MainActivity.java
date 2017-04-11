bbf1d9a126e30b3fa79a4f400f28e22ce75a41ce
Yebon Lee <bbong0722@e-mirim.hs.kr>
2017-04-11 19:24:32 +0900

    #13 각 버튼 누르면 사진 바뀜 구현

--- app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
@@ -25,22 +25,27 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.button1:
                 Log.d(TAG, "버튼1클릭");
+                mImageViewFriendsVisual.setImageResource(R.drawable.chu_1);
                 Toast.makeText(this, "썸바디 헬ㅍ믜~~~!!~!~!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
                 Log.d(TAG, "버튼2클릭");
+                mImageViewFriendsVisual.setImageResource(R.drawable.chu_2);
                 Toast.makeText(this, "모해~? 지금은~?!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
                 Log.d(TAG, "버튼3클릭");
+                mImageViewFriendsVisual.setImageResource(R.drawable.chu_3);
                 Toast.makeText(this, "여기는 지옥인걸까....?", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
                 Log.d(TAG, "버튼4클릭");
+                mImageViewFriendsVisual.setImageResource(R.drawable.chu_4);
                 Toast.makeText(this, "하ㅣㅎ갛가학핳각학", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
                 Log.d(TAG, "버튼5클릭");
+                mImageViewFriendsVisual.setImageResource(R.drawable.chu_5);
                 Toast.makeText(this, "지려따리~ 오져따리~ ! ! !", Toast.LENGTH_SHORT).show();
                 break;
             default:
