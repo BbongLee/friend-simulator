72234a868cb8f7d9a219e18b634fbf6ef63e0f17
Yebon Lee <bbong0722@e-mirim.hs.kr>
2017-04-11 20:46:39 +0900

    #18 모든 버튼 음성 나오게 하기!

--- app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/friendsimulatorbbonglee/imchung/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.friendsimulatorbbonglee.imchung;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,43 +11,69 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public final static String TAG = "Blue:Main";
     private ImageView mImageViewFriendsVisual;
+    private MediaPlayer mMediaPlayer; //m : 멤버변수
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //activity를 처음 생성할때!
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate 메소드 호출!");
         setContentView(R.layout.activity_main);
         mImageViewFriendsVisual = (ImageView) findViewById(R.id.friend_visual);
         Log.d(TAG, "activity_main 레이아웃 세팅!");
+        mMediaPlayer = MediaPlayer.create(this, R.raw.a01);
+        mMediaPlayer.setLooping(false);
+        mMediaPlayer.start();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.pause(); //멈추기
+        mMediaPlayer.stop(); //빼기
+    }
+
     void onClick(View view){
         Log.d(TAG, "onClick 메소드 호출!");
         Log.d(TAG, "클릭된 뷰아이디!"+ view.getId());
+
+        if(mMediaPlayer.isPlaying()){
+            mMediaPlayer.pause();
+        }
         switch (view.getId()){
             case R.id.button1:
                 Log.d(TAG, "버튼1클릭");
                 mImageViewFriendsVisual.setImageResource(R.drawable.chu_1);
                 Toast.makeText(this, "썸바디 헬ㅍ믜~~~!!~!~!", Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.a01);
+                mMediaPlayer.start();
                 break;
             case R.id.button2:
                 Log.d(TAG, "버튼2클릭");
                 mImageViewFriendsVisual.setImageResource(R.drawable.chu_2);
                 Toast.makeText(this, "모해~? 지금은~?!", Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.a02);
+                mMediaPlayer.start();
                 break;
             case R.id.button3:
                 Log.d(TAG, "버튼3클릭");
                 mImageViewFriendsVisual.setImageResource(R.drawable.chu_3);
                 Toast.makeText(this, "여기는 지옥인걸까....?", Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.a03);
+                mMediaPlayer.start();
                 break;
             case R.id.button4:
                 Log.d(TAG, "버튼4클릭");
                 mImageViewFriendsVisual.setImageResource(R.drawable.chu_4);
                 Toast.makeText(this, "하ㅣㅎ갛가학핳각학", Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.a04);
+                mMediaPlayer.start();
                 break;
             case R.id.button5:
                 Log.d(TAG, "버튼5클릭");
                 mImageViewFriendsVisual.setImageResource(R.drawable.chu_5);
                 Toast.makeText(this, "지려따리~ 오져따리~ ! ! !", Toast.LENGTH_SHORT).show();
+                mMediaPlayer = MediaPlayer.create(this, R.raw.a05);
+                mMediaPlayer.start();
                 break;
             default:
                 Log.d(TAG, "모르는버튼클릭");
